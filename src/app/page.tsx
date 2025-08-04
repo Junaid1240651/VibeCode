a6b17abf34c3c8f7ac3e21a053ad374d70cbd364
SDE1Junaid <junaid@akratech.in>
2025-08-04 14:02:34 +0530

    05: setup AI-Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 "use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
-import { useQuery, } from '@tanstack/react-query'
+import { useMutation, } from '@tanstack/react-query'
 import React from 'react'
+import { toast } from 'sonner'
 
 const Home = () => {
+  const [value, setValue] = React.useState<string>('');
   const trpc = useTRPC()
-  const { data } = useQuery(trpc.hello.queryOptions({ text: 'world' }))
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success('Background job has been started')
+    },
+    onError: () => {
+      console.log('Something is wrong')
+    }
+  }))
   return (
-    <div className='font-bold text-rose-500'>{ JSON.stringify(data)}</div>
+    <div className='font-bold text-rose-500'>
+      <Input value={value} onChange={(e)=> setValue(e.target.value)}/>
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value })}>
+        {invoke.isPending ? 'Loading...' : 'Click Me'}
+      </Button>
+    </div>
   )
 }
 
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from "./client";
+import {openai, createAgent,gemini } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+    async ({ event }) => {
+        // Create a new agent with a system prompt (you can add optional tools, too)
+        const writer = createAgent({
+            name: "summerizer",
+            system: "write an intro",
+            model: gemini({ model: "gemini-2.0-flash-lite" }),
+        });
+        const { output } = await writer.run(
+            `summerizer the following text and write an intro : ${event.data.value}`,
+        );
+        console.log(output);
+        
+        return { message: output }
     },
 );
