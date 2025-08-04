bf15f08ae4f2e65fa29f6ec4b5cad3caed915842
SDE1Junaid <junaid@akratech.in>
2025-08-04 11:16:31 +0530

    03: TRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+import { useTRPC } from '@/trpc/client'
+import { useQuery, } from '@tanstack/react-query'
 import React from 'react'
 
 const Home = () => {
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.hello.queryOptions({ text: 'world' }))
   return (
-    <div className='font-bold text-rose-500'>Home</div>
+    <div className='font-bold text-rose-500'>{ JSON.stringify(data)}</div>
   )
 }
 
